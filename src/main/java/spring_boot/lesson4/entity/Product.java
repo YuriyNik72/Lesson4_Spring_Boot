23d6919c1cf0c8a234089c40e56e5cfed2a55779
Yuriy <yurgennik@gmail.com>
2021-08-30 22:25:30 +0500

    add

--- src/main/java/spring_boot/lesson4/entity/Product.java
+++ src/main/java/spring_boot/lesson4/entity/Product.java
@@ -1,4 +1,33 @@
 package spring_boot.lesson4.entity;
 
 public class Product {
+    private int id;
+    private String title;
+    private int cost;
+//    private String[] programmingLanguages;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
 }
--- src/main/java/spring_boot/lesson4/repositories/ProductRepository.java
+++ src/main/java/spring_boot/lesson4/repositories/ProductRepository.java
@@ -1,4 +1,26 @@
 package spring_boot.lesson4.repositories;
 
+import org.springframework.stereotype.Component;
+import spring_boot.lesson4.entity.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class ProductRepository {
+    private static int count;
+    private List<Product> productList=new ArrayList<>();
+
+    public Product getProductById(int id) {
+        return this.productList.stream().filter(product -> product.getId() == id)
+                .findFirst().orElseGet(() -> {return null;});
+    }
+
+    public void addNewProduct(Product product){
+        product.setId(count++);
+        this.productList.add(product);
+    }
+    public List<Product> getAllProduct(){
+        return this.productList;
+    }
 }
